3a569931336c7dbb4983a9d356123bcd54513d25
Static_Carillion_RAO <ryanorlando02@gmail.com>
2023-05-18 12:35:25 +0530

    Changed requirements.txt

--- get_files.py
+++ get_files.py
@@ -7,5 +7,6 @@ direc = "adverts"
 files = os.listdir(direc)
 files = [f for f in files if os.path.isfile(direc+'/'+f)] #Filtering only the files.
 
+# return a random file
 def fetch_file():
     return random.choice(files)
