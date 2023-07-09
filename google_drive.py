1565863b834217d289ebcb89cc06d89f4258bd47
Static_Carillion_RAO <ryanorlando02@gmail.com>
2023-07-09 15:22:40 +0530

    changes in requirements and handles duplicate files in drive

--- google_drive.py
+++ google_drive.py
@@ -52,6 +52,46 @@ def folder_there(flow):
 
 #--------------------------------------------------------------------------------------------------
 
+def file_there(flow, folder_id, filename):
+
+    global folder_name
+
+    creds = flow.credentials
+
+    try:
+        # create drive api client
+        service = build('drive', 'v3', credentials=creds)
+
+        # pylint: disable=maybe-no-member
+        results = service.files().list(
+            q=f"'{folder_id}' in parents",          # man the query needs to have folder name/id in ''
+            pageSize=10, fields="nextPageToken, files(id, name)").execute()
+        files = results.get('files', [])
+
+        print(files)
+
+        flag = 1
+
+        if not files:
+            print('No files found.')
+            return flag
+        
+        else:
+            print('Folders:')
+            for item in files:
+                
+                print(u'{0} ({1})'.format(item['name'], item['id']))
+
+                if item['name'] == filename:
+                    return item['id']
+            return flag
+
+    except HttpError as error:
+        print(F'An error occurred: {error}')
+        return 0
+
+#--------------------------------------------------------------------------------------------------
+
 def create_folder(flow):
 
     global folder_name
@@ -91,6 +131,8 @@ def upload_to_folder(flow, folder_id, file):
 
     file_name = secure_filename(file.filename)
 
+    file_id = file_there(flow, folder_id, file_name)
+
     try:
     # create drive api client
         service = build('drive', 'v3', credentials=creds)
@@ -104,10 +146,17 @@ def upload_to_folder(flow, folder_id, file):
         media = MediaIoBaseUpload(f, mimetype='image/jpeg', resumable=True)
 
         # pylint: disable=maybe-no-member
-        file = service.files().create(body=file_metadata, media_body=media,
-                                      fields='id').execute()
-        print(F'File ID: "{file.get("id")}".')
-        return file.get('id')
+        if file_id == 1:
+            file = service.files().create(body=file_metadata, media_body=media,
+                                        fields='id').execute()
+            print(F'File ID: "{file.get("id")}".')
+            return file.get('id')
+        
+        else:
+            # update the file content given the fiel_id
+            file = service.files().update(fileId=file_id, media_body=media).execute()
+            print(F'File ID: "{file.get("id")}".')
+            return file.get('id')
 
     except HttpError as error:
         print(F'An error occurred: {error}')
